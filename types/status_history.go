c99bfdc5175b4957d44ab7906b5db032805f4bf6
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2019-05-08 19:04:32 +0200

    switch type from extension_attributes

--- types/status_history.go
+++ types/status_history.go
@@ -1,13 +1,13 @@
 package types
 
 type StatusHistory struct {
-	Comment             string              `json:"comment,omitempty"`
-	CreatedAt           string              `json:"created_at,omitempty"`
-	EntityID            int                 `json:"entity_id,omitempty"`
-	EntityName          string              `json:"entity_name,omitempty"`
-	IsCustomerNotified  int                 `json:"is_customer_notified,omitempty"`
-	IsVisibleOnFront    int                 `json:"is_visible_on_front,omitempty"`
-	ParentID            int                 `json:"parent_id,omitempty"`
-	Status              string              `json:"status,omitempty"`
-	ExtensionAttributes ExtensionAttributes `json:"extension_attributes,omitempty"`
+	Comment             string                 `json:"comment,omitempty"`
+	CreatedAt           string                 `json:"created_at,omitempty"`
+	EntityID            int                    `json:"entity_id,omitempty"`
+	EntityName          string                 `json:"entity_name,omitempty"`
+	IsCustomerNotified  int                    `json:"is_customer_notified,omitempty"`
+	IsVisibleOnFront    int                    `json:"is_visible_on_front,omitempty"`
+	ParentID            int                    `json:"parent_id,omitempty"`
+	Status              string                 `json:"status,omitempty"`
+	ExtensionAttributes map[string]interface{} `json:"extension_attributes,omitempty"`
 }
