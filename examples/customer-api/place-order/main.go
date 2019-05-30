fc99fba859867fdec6b4be84a50c80b1ef38db0d
Dennis Hermsmeier <dennis.hermsmeier@gmail.com>
2019-05-30 09:47:40 +0200

    overwork example stuff

---
+++ examples/customer-api/place-order/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"github.com/hermsi1337/go-magento2/pkg/magento2"
+	"log"
+)
+
+func main() {
+	// initiate storeconfig
+	storeConfig := &magento2.StoreConfig{
+		Scheme:    "https",
+		HostName:  "localhost",
+		StoreCode: "default",
+	}
+	// initiate login payload
+	authenticationPaylod := &magento2.AuthenticationRequestPayload{
+		Username: "anonymous@my.m2.tld",
+		Password: "foobar!23",
+	}
+	// create a new apiClient
+	apiClient, err := magento2.NewCustomerApiClient(storeConfig, authenticationPaylod)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Obtained client: '%v'", apiClient)
+
+	// create empty card
+	cart, err := apiClient.NewCustomerCart()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Obtained cart: '%v'", cart)
+
+	// initialize items array
+	var products []magento2.Item
+
+	// add items to your items array
+	products = append(products, magento2.Item{
+		Sku: "123456",
+		Qty: 1,
+	})
+
+	// update your cart with the desired items
+	err = cart.AddItems(products)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Added product: '%v'", cart.Detailed.Items)
+
+	// define shipping address
+	sAddr := &magento2.Address{
+		City:      "FooCity",
+		Company:   "FooCompany",
+		Email:     "foo@bar.de",
+		Firstname: "Foo",
+		Lastname:  "Bar",
+		Postcode:  "1337",
+		CountryID: "DE",
+		Telephone: "1337 1337 1337",
+		Street:    []string{"foo", "street"},
+	}
+
+	// define billing address
+	bAddr := &magento2.Address{
+		City:      "FooCity",
+		Company:   "FooCompany",
+		Email:     "foo@bar.de",
+		Firstname: "Foo",
+		Lastname:  "Bar",
+		Postcode:  "1337",
+		CountryID: "DE",
+		Telephone: "1337 1337 1337",
+		Street:    []string{"foo", "street"},
+	}
+
+	// estimate shipping carrier for our cart
+	availableCarrier, err := cart.EstimateShippingCarrier(*sAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Obtained carrier: '%v'", availableCarrier)
+
+	// choose your desired carrier
+	desiredCarrier := availableCarrier[0]
+
+	log.Printf("Chosen carrier: '%v'", desiredCarrier)
+
+	// define addressinformation-payload for your cart
+	payLoad := &magento2.AddressInformation{
+		ShippingAddress:      *sAddr,
+		BillingAddress:       *bAddr,
+		ShippingMethodCode:   desiredCarrier.MethodCode,
+		ShippingCarrierCodes: desiredCarrier.CarrierCode,
+	}
+
+	// add shipping info to cart
+	err = cart.AddShippingInformation(*payLoad)
+	if err != nil {
+		panic(err)
+	}
+
+	// lets check what payment methods are available
+	paymentMethods, err := cart.EstimatePaymentMethods()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Obtained payment methods: '%v'", paymentMethods)
+
+	// choose your desired payment method
+	desiredPaymentMethod := paymentMethods[0]
+
+	log.Printf("Chosen payment method: '%v'", desiredPaymentMethod)
+
+	// create the order
+	order, err := cart.CreateOrder(desiredPaymentMethod)
+	if err != nil {
+		panic(err)
+	}
+
+	// Congrats, your order has been submitted
+	log.Printf("Your oder has been submitted. OrderID: '%v'", order.ID)
+}
